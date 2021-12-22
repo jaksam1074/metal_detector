686fb2cfd11e8a9164c8dda6f34850095f3a50b9
jaksam1074 <33578278+jaksam1074@users.noreply.github.com>
2021-12-22 12:01:48 +0100

    Added french translation

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,5 +1,5 @@
-locales['en'] = {
-    ['something_detected'] = "~r~Quelque chose~s~ détecté ",
+locales['fr'] = {
+    ['something_detected'] = "~r~Quelque chose~s~ détecté",
     ['nothing_detected'] = "~g~Rien~s~ n'a été détecté",
     ['select_target'] = "Choisissez la cible",
     ['scan'] = "Scanner",
