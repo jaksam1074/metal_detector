9af8302c80e3f425af73e3d1aa23aa4dda78d19b
Youssef D <46886884+djabayy@users.noreply.github.com>
2021-12-22 01:20:40 +0100

    Create fr.lua - french translation

---
+++ locales/fr.lua
@@ -0,0 +1,7 @@
+locales['en'] = {
+    ['something_detected'] = "~r~Quelque chose~s~ détecté ",
+    ['nothing_detected'] = "~g~Rien~s~ n'a été détecté",
+    ['select_target'] = "Choisissez la cible",
+    ['scan'] = "Scanner",
+    ['stop'] = "Stop",
+}
