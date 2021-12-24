5cf178167c9d989a090088c1517deb509dd709a7
xProxyRed <xproxyred@gmail.com>
2021-12-24 12:38:50 +0100

    Create de.lua
    german translation added

---
+++ locales/de.lua
@@ -0,0 +1,7 @@
+locales['de'] = {
+    ['something_detected'] = "~r~Etwas~s~ festgestellt",
+    ['nothing_detected'] = "~g~Nichts~s~ festgestellt",
+    ['select_target'] = "Ziel auswählen",
+    ['scan'] = "Scan",
+    ['stop'] = "Stop",
+}
